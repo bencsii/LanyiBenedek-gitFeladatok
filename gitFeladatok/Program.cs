042602b3c30da11ea6997259450c5d70b38dfc9a
bencsii <benicke001@gmail.com>
2024-12-18 18:24:23 +0100

    Add project files.

---
+++ gitFeladatok/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitFeladatok
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        static void f01(int[] tomb)
+        {
+            for(int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.Next(1,51);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
